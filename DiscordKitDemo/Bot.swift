d60bf87b0a12845cae0ced639edc9ebf439a3df2
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-12-13 20:03:06 +0800

    feat: add calculator command example
    handling of the calculator command is still quite messy, it might be improved in the future.

--- DiscordKitDemo/Bot.swift
+++ DiscordKitDemo/Bot.swift
@@ -45,6 +45,7 @@ public struct Bot {
     /// commands such as user or message commands might be added in the future.
     private static func registerSlashCommands() async throws {
         try await bot.registerApplicationCommands(guild: ProcessInfo.processInfo.environment["COMMAND_GUILD_ID"]) {
+            // MARK: hello - Basic command which uses a variety of options
             NewAppCommand("hello", description: "Get a nice hello message") {
                 StringOption("name", description: "Your beautiful name")
                 IntegerOption("age", description: "How old (in years) are you?")
@@ -64,16 +65,57 @@ public struct Bot {
                 }
             }
 
+            // MARK: time - Simple command without any options
             NewAppCommand("time", description: "What time is it???") { interaction in
                 try? await interaction.reply("Today's <t:\(Int(Date().timeIntervalSince1970)):F>")
             }
 
+            // MARK: choose - Demonstrates simple sub-commands
             NewAppCommand("choose", description: "Make a choice, and I'll tell you if I approve of it") {
                 SubCommand("yes", description: "How about yes?")
                 SubCommand("no", description: "I think not")
             } handler: { interaction in
-                let choice = interaction.isSubOption(name: "yes") ?? false
+                let choice = interaction.subOption(name: "yes") != nil
                 try? await interaction.reply("You said \(choice ? "yes" : "no"), and I \(Bool.random() ? "" : "dis")agree!")
+            }
+
+            // MARK: calculator - A complex command with nested options
+            NewAppCommand("calculator", description: "Need help crunching numbers? Just use this command!") {
+                // Sub-commands can be defined like so, and even support nested options!
+                SubCommand("plus", description: "Add two numbers") {
+                    NumberOption("lhs", description: "First number to add", required: true)
+                    NumberOption("rhs", description: "Second number to add", required: true)
+                }
+                SubCommand("minus", description: "Subtract one number from the other") {
+                    NumberOption("lhs", description: "Number to subtract from", required: true)
+                    NumberOption("rhs", description: "Number to subtract", required: true)
+                }
+                SubCommand("times", description: "Multiply the first number with the other") {
+                    NumberOption("lhs", description: "Base value to multiply", required: true)
+                    NumberOption("rhs", description: "Multiplier/Factor to multiply base value by", required: true)
+                }
+                SubCommand("divide", description: "Divide the first number by the second") {
+                    NumberOption("lhs", description: "Quotient (number to divide)", required: true)
+                    NumberOption("rhs", description: "Divisor", required: true)
+                }
+            } handler: { interaction in
+                if let opt = interaction.subOption(name: "plus"),
+                   let lhs: Double = opt["lhs"]?.value(),
+                   let rhs: Double = opt["rhs"]?.value() {
+                    try? await interaction.reply("`\(lhs) + \(rhs) = \(lhs + rhs)`")
+                } else if let opt = interaction.subOption(name: "minus"),
+                          let lhs: Double = opt["lhs"]?.value(),
+                          let rhs: Double = opt["rhs"]?.value() {
+                    try? await interaction.reply("`\(lhs) - \(rhs) = \(lhs - rhs)`")
+                } else if let opt = interaction.subOption(name: "times"),
+                          let lhs: Double = opt["lhs"]?.value(),
+                          let rhs: Double = opt["rhs"]?.value() {
+                    try? await interaction.reply("`\(lhs) × \(rhs) = \(lhs * rhs)`")
+                } else if let opt = interaction.subOption(name: "divide"),
+                          let lhs: Double = opt["lhs"]?.value(),
+                          let rhs: Double = opt["rhs"]?.value() {
+                    try? await interaction.reply("`\(lhs) ÷ \(rhs) = \(lhs / rhs)`")
+                }
             }
         }
     }
